1f3eb657607bc40161cc17fb57c87fb33a50a831
igors48 <nmds48@gmail.com>
2022-06-09 08:03:52 +0300

    initial

--- src/main/java/com/github/igors48/App.java
+++ src/main/java/com/github/igors48/App.java
@@ -1,13 +1,44 @@
 package com.github.igors48;
 
-/**
- * Hello world!
- *
- */
-public class App 
+import jssc.SerialPort;
+import jssc.SerialPortEvent;
+import jssc.SerialPortException;
+import jssc.SerialPortList;
+
+public class App
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main(String[] args) {
+        String[] names = SerialPortList.getPortNames();
+        System.out.println(names);
+
+        SerialPort port = new SerialPort("/dev/ttyUSB0");
+
+        try {
+            port.openPort();
+            //Miniterm on /dev/ttyUSB0  115200,8,N,1 ---
+            port.setParams(
+                    SerialPort.BAUDRATE_115200,
+                    SerialPort.DATABITS_8,
+                    SerialPort.STOPBITS_1,
+                    SerialPort.PARITY_NONE
+            );
+
+            port.addEventListener((SerialPortEvent event) -> {
+                if (event.isRXCHAR()) {
+                    try {
+                        String s = port.readString();
+                        System.out.print(s);
+                    } catch (SerialPortException ex) {
+                        throw new RuntimeException(ex);
+                    }
+                }
+            });
+            // https://www.codeproject.com/Tips/801262/Sending-and-receiving-strings-from-COM-port-via-jS
+            boolean result = port.writeString("Hurrah!");
+            System.out.println("###Result " + result);
+        } catch (SerialPortException e) {
+            throw new RuntimeException(e);
+        }
     }
+
 }
